b98fc66c4dcf1166f95fcf763f1dbf95b280a172
Mariana Hristozkova <kukita@abv.bg>
2021-12-05 20:27:42 +0200

    Tested methods for open addressing

--- HashTableOpenAddressingStrategy/Program.cs
+++ HashTableOpenAddressingStrategy/Program.cs
@@ -11,6 +11,27 @@ namespace HashTableOpenAddressingStrategy
         static void Main(string[] args)
         {
             MyHashTableOpenAddressing hashTable1 = new MyHashTableOpenAddressing();
+            hashTable1.Insert(6, "A");
+            hashTable1.Insert(8, "B");
+            hashTable1.Insert(11, "C");
+
+            hashTable1.Print();
+
+            MyHashTableOpenAddressing hashTable2 = new MyHashTableOpenAddressing();
+            hashTable2.QuadraticHashInsert(6, "A");
+            hashTable2.QuadraticHashInsert(8, "B");
+            hashTable2.QuadraticHashInsert(11, "C");
+
+            hashTable2.Print();
+
+            MyHashTableOpenAddressing hashTable3 = new MyHashTableOpenAddressing();
+            hashTable3.DoubleHashInsert(6, "A");
+            hashTable3.DoubleHashInsert(8, "B");
+            hashTable3.DoubleHashInsert(11, "C");
+
+            hashTable3.Print();
+
+            Console.ReadKey();
         }
     }
 }
